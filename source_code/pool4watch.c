bfcd53368ffb8de5932639af7e9650c3f10c637b
zhangly <dear531@21cn.com>
2014-07-03 22:37:25 +0800

    add debug step condition commands and finish etc

--- source_code/pool4watch.c
+++ source_code/pool4watch.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+int watch_variable_another(void)
+{
+	int k;
+	for (k = 0; k < 5; k++)
+	{
+		fprintf(stdout, "k:%d\n", k);
+	}
+	return 0;
+}
 int main(int argc, char *argv[])
 {
 	int i, j = 0;
+
+	watch_variable_another();
+
 	for (i = 0; i < 5; i++)
 	{
 		fprintf(stdout, "i :%d,", i);
 		j++;
 		fprintf(stdout, "j :%d\n", j);
 	}
+
 	return 0;
 }
